3878f788a9c0e1ce5a9e650b2847b4501c44cd1b
Preeti811 <preeti.20204145@mnnit.ac.in>
2022-04-23 16:37:51 +0530

    Second Activity

--- app/src/main/java/com/example/quarantin/MainActivity.java
+++ app/src/main/java/com/example/quarantin/MainActivity.java
@@ -1,14 +1,87 @@
+//package com.example.quarantin;
+//
+//import androidx.appcompat.app.AppCompatActivity;
+//
+//import android.content.Intent;
+//import android.os.Bundle;
+//import android.widget.Button;
+//
+//import com.google.firebase.auth.FirebaseAuth;
+//import com.google.firebase.auth.FirebaseUser;
+//
+//public class MainActivity extends AppCompatActivity {
+//
+//    //my code
+//
+//    Button btnLogOut;
+//    FirebaseAuth mAuth;
+//
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_main);
+//
+//        btnLogOut = findViewById(R.id.btnLogout);
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        btnLogOut.setOnClickListener(view ->{
+//            mAuth.signOut();
+//            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//        });
+//
+//    }
+//
+//    @Override
+//    protected void onStart() {
+//        super.onStart();
+//        FirebaseUser user = mAuth.getCurrentUser();
+//        if (user == null){
+//            startActivity(new Intent(MainActivity.this, LoginActivity.class));
+//        }
+//    }
+//
+//
+//    //my code
+//
+//
+//
+//
+////    @Override
+////    public void onCreate(Bundle savedInstanceState) {
+////        super.onCreate(savedInstanceState);
+////        setContentView(R.layout.activity_main);
+////    }
+//
+//}
+
+
 package com.example.quarantin;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 
+import java.util.Objects;
+import java.util.Timer;
+import java.util.TimerTask;
+
 public class MainActivity extends AppCompatActivity {
+    Timer timer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 1000);
+        Objects.requireNonNull(getSupportActionBar()).setBackgroundDrawable(new ColorDrawable(getResources().getColor(R.color.teal_700)));
     }
 }
